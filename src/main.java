17f415c0032c8c5d12cbe99cc5058626b69d1abc
abdullah-aladham <abdullah.aladham@gmail.com>
2024-01-10 23:33:34 +0200

    tried to write my first TestCase

--- src/main.java
+++ src/main.java
@@ -1,6 +1,11 @@
+import org.testng.annotations.Test;
 
 public class main {
 
+	@Test
+	public static void opensite() {
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
